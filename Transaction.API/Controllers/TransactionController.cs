cb180bbcfe0967ddbe8d0c52f1e4b42218d3cfda
lalji solanki <laljisolanki80@yahoo.in>
2019-08-09 18:02:27 +0530

    WIP:CreatNewTransaction added

---
+++ Transaction.API/Controllers/TransactionController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Transaction.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Transaction")]
+    public class TransactionController : Controller
+    {
+        [Route("CreateTransaction")]
+        [HttpPost]
+        public async Task<IActionResult> CreateNewTransaction ([FromBody]int AccountId, string Name, int Amount)
+        {
+            if (AccountId==null)
+            {
+
+                return BadRequest();
+            }
+               return Ok("done");      
+        }
+
+      
+    }
+}
