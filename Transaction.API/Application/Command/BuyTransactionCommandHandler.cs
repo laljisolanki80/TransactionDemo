823af32afc9eb4172cf20406358c4b2e7ab97764
Akshay Kothari <akshay.jbspl@gmail.com>
2019-08-10 19:36:51 +0530

    add by Akshay

--- Transaction.API/Application/Command/BuyTransactionCommandHandler.cs
+++ Transaction.API/Application/Command/BuyTransactionCommandHandler.cs
@@ -24,21 +24,16 @@ namespace Transaction.API.Application.Command
             _mediator = mediator;
             _logger = logger;
         }
-
         public Task<bool> Handle(BuyTransactionCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            var buyer = new Buyer(request.Id.ToString(), request.Price, request.Quantity);
+            foreach (var item in request.BuyItem)
+            {
+                buyer.
+            }
+            // _logger.LogInformation("----- Creating Order - Order: {@Order}",buyer);
+
         }
-        //public Task<bool> Handle(BuyTransactionCommand request, CancellationToken cancellationToken)
-        //{
-        //    //throw new NotImplementedException();
-        //    var buyer = new Buyer(request.Id, request.Price, request.Quantity);
-        //    foreach (var item in request.Buy)
-        //    {
-
-        //    }
-        //    // _logger.LogInformation("----- Creating Order - Order: {@Order}",buyer);
-
-        //}
     }
 }
