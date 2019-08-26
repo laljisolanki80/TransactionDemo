b333bf2d4821be0318989ee75bc6a405953ce45d
Akshay Kothari <akshay.jbspl@gmail.com>
2019-08-26 13:38:15 +0530

    add BizResponse in domainLayer Add by akshay

---
+++ Transaction.Domain/AggreagatesModels/Aggregate/BizResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Transaction.Domain.Enum;
+
+namespace Transaction.Domain.AggreagatesModels.Aggregate
+{
+    public class BizResponse
+    {
+        public enErrorCode ErrorCode { get; set; }
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+    }
+}
